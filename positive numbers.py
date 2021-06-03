d3b564313eb18815067effdda97379d60f91af47
Jaxkirat <77850299+Jaxkirat@users.noreply.github.com>
2021-06-03 20:00:01 +0530

    positive_numbers
    An a given input of two different lists with positive and negative integers. This code gives the positive number from the lists as output.

---
+++ positive numbers.py
@@ -0,0 +1,18 @@
+print("Output for list1:")
+
+###input list 1
+list1 = [12, -7, 5 , 64, -14]
+
+for a in list1 :
+    if a > 0:
+        print (a)
+
+print("Output for list2:") 
+###input list 2   
+list2 = [12, 14, -95, 3]
+
+for i in list2 :
+    if i > 0: 
+        print(i)
+    
+    
